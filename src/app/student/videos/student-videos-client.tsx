2330ef7b93cc15defec3f0e45179aba6836a73ac
Coach <coach@cs2.local>
2026-09-10 01:54:33 +0200

    fix: jednolity crop 130% aby ukryc UI YT bez czarnej kreski i zachowac max kadru
    - poprzedni asymetryczny 100%x122% psul aspect 16:9 -> letterbox czarna kreska na dole
    - zmiana na jednolity 130% center translate = crop 11.5% kazda krawedz, widoczne 77% filmu, chowa top-bar/more/watermark bez czarnej linii
    - zachowuje proporcje 16:9, brak letterbox, vq=hd720 dla wysokiej jakosci, thumbnail overlay do onLoad chowa srodkowy play

--- src/app/student/videos/student-videos-client.tsx
+++ src/app/student/videos/student-videos-client.tsx
@@ -402,7 +402,7 @@ export function StudentVideosClient({ initialSessions, initialProgress }: Studen
                           <iframe
                             src={`https://www.youtube-nocookie.com/embed/${ytId}?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&playsinline=1&iv_load_policy=3&disablekb=1&fs=0&cc_load_policy=0&showinfo=0&enablejsapi=0&vq=hd720`}
                             className="absolute pointer-events-none"
-                            style={{ left: 0, top: '-18%', width: '100%', height: '122%' }}
+                            style={{ left: '50%', top: '50%', width: '130%', height: '130%', transform: 'translate(-50%,-50%)' }}
                             allow="autoplay; encrypted-media"
                             title=""
                             tabIndex={-1}
